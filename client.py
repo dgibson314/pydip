a354c60911886e0e3bb109a55a9f229db2b90c1e
David <dgibson314@gmail.com>
2019-06-04 17:20:34 -0400

    Now able to connect, albeit has to continue to recv messages from server

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ class BaseClient():
         self.port = port
         self.sock = None
         self.connected = False
+        self.name = 'BaseClient'
         self.version = 1
 
     def connect(self):
@@ -35,7 +36,7 @@ class BaseClient():
     def get_header(self):
         if (self.connected):
             header = self.sock.recv(4)
-            (msg_type, pad, msg_len) = struct.unpack('!hhl')
+            (msg_type, pad, msg_len) = struct.unpack('!hhl', header)
             return (msg_type, msg_len)
         else:
             raise Exception("Not connected to server.")
@@ -45,30 +46,46 @@ class BaseClient():
             (msg_type, msg_len) = self.get_header()
 
             bufsize = 1024
-            msg = ''
-            while (msg_len > 0):
-                if (msg_len > bufsize):
-                    msg += self.sock.recv(bufsize)
-                    msg_len -= bufsize
-                else:
-                    msg += self.sock.recv(msg_len)
-                    msg_len = 0
-
-            return (msg_type, msg)
-        except: print("Unable to get message")
+            bytes_recvd = 0
+            msg = []
+            while (bytes_recvd < msg_len):
+                chunk = self.sock.recv(min(msg_len - bytes_recvd, bufsize))
+                if chunk == b'':
+                    raise RuntimeError("socket connection broken")
+                msg.append(chunk)
+                bytes_recvd = bytes_recvd + len(chunk)
+            return b''.join(msg)
+        except Exception as e:
+            print(e)
 
     def write(self, message, msg_type):
-        byte_length = max(len(message) // 2, 2)
-        header = struct.pack('!hhl', msg_type, 0, byte_length)
+        byte_length = len(message)
+        header = struct.pack('!bxh', msg_type, byte_length)
+
+        print("HEADER: \t", header)
+        print("MESSAGE: \t", message)
+
         message = header + message
+        print(len(message))
         self.sock.send(message)
 
     def send_obs(self):
+        msg = Message(OBS).pack()
+        self.write(msg, 2)
+
+    def send_nme(self):
         pass
+
     def send_iam(self):
         pass
 
     def send_initial_msg(self):
-        msg = struct.pack('!ll', self.version, 0xDA10)
+        msg = struct.pack('!HH', self.version, 0xDA10)
         self.write(msg, 0)
 
+b = BaseClient()
+b.connect()
+b.send_initial_msg()
+b.send_obs()
+while True:
+    b.sock.recv(1024)

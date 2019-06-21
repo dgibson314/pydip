62d1936dedc6f37cff45c3c45f00c2200ab62c75
David <dgibson314@gmail.com>
2019-06-21 16:47:55 -0400

    Added method to update supply center ownership with SCO message

--- gameboard.py
+++ gameboard.py
@@ -1,14 +1,33 @@
 class Gameboard():
     '''
     Stores info about current turn and current unit positions.
-        - powers        List of powers
-        - provinces     Dictionary of powers mapped to home SCs, and a list on non-SCs
-        - adjacencies   
+
+    The following instance variables refer to the initial game state, and will
+    remain unchanged throughout the game.
+    - powers        List of powers
+    - home_centers  Dictionary of powers mapped to their home SCs
+    - adjacencies   Mimics the structure of MDF adjacency list, e.g. [[prov_adj][prov_adj]...],
+                    where [prov_adj] is of the form 
+                    [province [unit_type adj_prov adj_prov...]...]...].
+                    See the DAIDE Message Syntax section on the MDF message
+                    for more details.
+
+    The following instance variables are updated as the game progresses, 
+    (probably) by being passed NOW and SCO messages from the DAIDE server.
+    - supply_centers    Mapping from powers to a list of SCs they have 
+                        after each Fall Retreat turn
+    - units             
+
     '''
     def __init__(self, MDF_message):
         self.powers = []
-        self.provinces = {}
+        self.home_centers = {}
         self.adjacencies = {}
+
+        self.supply_centers = {}
+        self.units = {}
+        self.year = None
+        self.season = None
 
         folded_msg = MDF_message.fold()
 
@@ -20,13 +39,7 @@ class Gameboard():
         sc_section = folded_msg[2][0]
         for sc_lst in sc_section:
             power = sc_lst[0]
-            self.provinces[power] = sc_lst[1:]
-
-        # Adding non-SCs
-        self.provinces['Non-SC'] = []
-        non_sc_section = folded_msg[2][1]
-        for province in non_sc_section:
-            self.provinces['Non-SC'].append(province)
+            self.home_centers[power] = sc_lst[1:]
 
         # Adding adjacencies 
         adjacencies = folded_msg[3]
@@ -41,3 +54,16 @@ class Gameboard():
                     pass
                 else:
                     self.adjacencies[province][unit_type] = adj[1:]
+
+    def update_supply_centers(self, SCO_message):
+        ''' Updates the current supply center ownership by traversing
+        an SCO message from DAIDE server. Unowned centers are listed against
+        the power name UNO.
+        '''
+        folded_SCO = SCO_message.fold()
+        for position in folded_SCO[1:]:
+            power = position[0]
+            centers = position[1:]
+            self.supply_centers[power] = []
+            for center in centers:
+                self.supply_centers[power].append(center)

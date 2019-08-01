8dd4e696596fe6d631335f9e4060aa3a7719dbdf
David Gibson <dgibson314@gmail.com>
2019-08-01 09:00:11 -0400

    Removing some whitespace to make Syntastic shut up

--- init.py
+++ init.py
@@ -1,29 +1,29 @@
 categories = {
-    0x40 : 'BRACKET',
-    0x41 : 'POWER',
-    0x42 : 'UNIT TYPE',
-    0x43 : 'ORDER',
-    0x44 : 'ORDER NOTE',
-    0x45 : 'RESULT',
-    0x46 : 'COAST',
-    0x47 : 'PHASE',
-    0x48 : 'COMMAND',
-    0x49 : 'PARAMETER',
-    0x5A : 'PRESS',
-    0x4B : 'TEXT',
-    0x50 : 'PROVINCE',
-    0x58 : 'RESERVED',
+    0x40: 'BRACKET',
+    0x41: 'POWER',
+    0x42: 'UNIT TYPE',
+    0x43: 'ORDER',
+    0x44: 'ORDER NOTE',
+    0x45: 'RESULT',
+    0x46: 'COAST',
+    0x47: 'PHASE',
+    0x48: 'COMMAND',
+    0x49: 'PARAMETER',
+    0x5A: 'PRESS',
+    0x4B: 'TEXT',
+    0x50: 'PROVINCE',
+    0x58: 'RESERVED',
 }
 
 province_categories = {
-    0x50 : 'Inland non-SC',
-    0x51 : 'Inland SC',
-    0x52 : 'Sea non-SC',
-    0x53 : 'Sea SC',
-    0x54 : 'Coastal non-SC',
-    0x55 : 'Coastal SC',
-    0x56 : 'Bicoastal non-SC',
-    0x57 : 'Bicoastal SC',
+    0x50: 'Inland non-SC',
+    0x51: 'Inland SC',
+    0x52: 'Sea non-SC',
+    0x53: 'Sea SC',
+    0x54: 'Coastal non-SC',
+    0x55: 'Coastal SC',
+    0x56: 'Bicoastal non-SC',
+    0x57: 'Bicoastal SC',
 }
 
 ###################################
@@ -34,4 +34,3 @@ RM = 0x1
 DM = 0x2
 FM = 0x3
 EM = 0x4
-
